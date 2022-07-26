dfa87f147c647e4db2f54d28f27f71b322b84ecb
subsplashdev <109565016+subsplashdev@users.noreply.github.com>
2022-07-26 14:28:05 -0500

    Update slide-pricing.js

--- slide-pricing.js
+++ slide-pricing.js
@@ -163,7 +163,7 @@
         $('.pricing-card').addClass('active-4 display-hidden');
       }
 
-       else if($('#ranger').val() > 1000 && $('#ranger').val() < 1005){
+       else if($('#ranger').val() > 1000 && $('#ranger').val() < 1100){
         
         $('.pricing-card').removeClass('active-1 active-2 active-3 active-4 active-5');
         $('.pricing-card').addClass('active-5 display-hidden');
